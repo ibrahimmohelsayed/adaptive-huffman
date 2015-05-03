d39ed85b3a33679ca10c165e4fb1e5e4939f33f5
katzoo <katzoo@github.mail>
2015-05-03 12:11:51 +0200

    code import from amu repository

---
+++ js/huffman.js
@@ -0,0 +1,330 @@
+/**
+ * Adaptive Huffman encoder class.
+ *
+ * @param {number} alphabetSize Alphabet size for adaptive Huffman encoder.
+ * @returns {{encode: Function, getRoot: Function}}
+ * @constructor
+ */
+var AdaptiveHuffman = function (alphabetSize) {
+    'use strict';
+
+    var addNode,
+        compInput = 0,
+        compOutput = 0,
+        encodeCharacter,
+        encoder,
+        findSymbolNode,
+        getHighestIdInBlock,
+        getHuffmanCode,
+        Helpers,
+        isTreeFull = false,
+        log,
+        maxSize,
+        nodes,
+        NYTNode,
+        root,
+        swapNodes,
+        symbols;
+
+    /**
+     * Helper functions.
+     *
+     * @type {{swapProperty: Function}}
+     */
+    Helpers = {
+        swapProperty: function (objA, objB, property) {
+            var tmp = objA[property];
+            objA[property] = objB[property];
+            objB[property] = tmp;
+        },
+        sendMessage: function (type, msg) {
+            if (window.CustomEvent) {
+                var event = new CustomEvent(type, {
+                    detail: {
+                        message: msg,
+                        time: new Date()
+                    },
+                    bubbles: true,
+                    cancelable: true
+                });
+
+                document.dispatchEvent(event);
+            }
+        }
+    };
+
+    /**
+     * Simple logger.
+     */
+    log = function () {
+        Helpers.sendMessage('log', Array.prototype.slice.call(arguments).join(' '));
+    };
+
+    /**
+     * Simple encoder.
+     */
+    encoder = function () {
+        Helpers.sendMessage('encode', Array.prototype.slice.call(arguments).join(''));
+    };
+
+    /**
+     * Private class representing a single node of Huffman tree.
+     *
+     * @param {?Node} parent Parent pointer.
+     * @param {?string} symbol Node's symbol.
+     * @param {number} id Node's id.
+     * @constructor
+     */
+    function Node(parent, symbol, id) {
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+        this.symbol = symbol;
+        this.weight = 0;
+        this.id = id;
+        log('New node created ( id:', this.id, 'symbol:', this.symbol, ')');
+    }
+
+    Node.prototype.increaseWeight = function () {
+        this.weight++;
+        log('Increasing weight for node', this.id);
+    };
+
+    Node.prototype.fixWeights = function () {
+        if(this.left && this.right) {
+            this.left.fixWeights();
+            this.right.fixWeights();
+            this.weight = this.left.weight + this.right.weight;
+        }
+    };
+
+    Node.prototype.setChildren = function (left, right) {
+        this.left = left;
+        this.right = right;
+        this.children = [];
+        if(left) {
+            this.children.push(left);
+        }
+        if(right) {
+            this.children.push(right);
+        }
+    };
+
+    log('Initializing Adaptive Huffman encoder with alphabet size of', alphabetSize);
+    symbols = {};
+    maxSize = alphabetSize * 2;
+    root = new Node(null, null, maxSize);
+    nodes = [root];
+    NYTNode = root;
+
+
+
+    /**
+     * Encodes single character.
+     *
+     * @param {string} symbol Single character to be encoded.
+     */
+    encodeCharacter = function (symbol) {
+        var encoderMsg = '',
+            highest,
+            isFirstSeen = false,
+            isInTree = symbols.hasOwnProperty(symbol),
+            node,
+            tmp;
+
+        // if symbol wasn't encoded yet
+        if (!isInTree) {
+            log('Symbol', symbol, 'not in tree yet');
+            isFirstSeen = true;
+            addNode(symbol);
+
+            // for the first character empty character is returned
+            if (Object.keys(symbols).length > 1) {
+                tmp = getHuffmanCode(NYTNode);
+                compOutput += tmp.length;
+                encoderMsg += tmp;
+            }
+            encoderMsg += symbol;
+        } else {
+            node = findSymbolNode(symbol);
+            log('Symbol', symbol, 'is already in the tree (', node.id, ')');
+            highest = getHighestIdInBlock(node.weight);
+            if (highest && highest.parent && (node.id !== highest.id) && (node.parent.id !== highest.id)) {
+                swapNodes(node, highest);
+            }
+        }
+
+        if (!isTreeFull || isInTree) {
+
+            compInput += symbols[symbol];
+
+            // update weight
+            node = findSymbolNode(symbol);
+            if (!isFirstSeen) {
+                tmp = getHuffmanCode(node);
+                compOutput += tmp.length;
+                encoderMsg += tmp;
+            }
+            encoder(encoderMsg);
+
+            node.increaseWeight();
+
+            // fix the tree
+            log('Fixing Huffman tree properties');
+            while (node !== root) {
+                node = node.parent;
+                highest = getHighestIdInBlock(node.weight);
+
+                if (highest && highest.parent && (node.id < highest.id) && (node.parent.id !== highest.id)) {
+                    swapNodes(node, highest);
+                    node = highest;
+                }
+                node.increaseWeight();
+            }
+        }
+    };
+
+    /**
+     * Finds node representing a given symbol.
+     *
+     * @param {string} symbol Symbol.
+     * @returns {Node} Node for a given symbol
+     */
+    findSymbolNode = function (symbol) {
+      for(var i = 0, l = nodes.length; i < l; i++) {
+          if(nodes[i].symbol === symbol) {
+              return nodes[i];
+          }
+      }
+    };
+
+    /**
+     * Swaps two nodes by replacing their properties.
+     *
+     * @param {Node} firstNode Instance of first node.
+     * @param {Node} secondNode Instance of second node.
+     */
+    swapNodes = function (firstNode, secondNode) {
+
+        // swap children parent pointers
+        if (firstNode.left) {
+            firstNode.left.parent = secondNode;
+        }
+        if (firstNode.right) {
+            firstNode.right.parent = secondNode;
+        }
+        if (secondNode.left) {
+            secondNode.left.parent = firstNode;
+        }
+        if (secondNode.right) {
+            secondNode.right.parent = firstNode;
+        }
+
+        // swap the rest of properties
+        ['left', 'right', 'parent', 'symbol', 'weight'].forEach(function (property) {
+            Helpers.swapProperty(firstNode, secondNode, property);
+        });
+
+        firstNode.setChildren(firstNode.left, firstNode.right);
+        secondNode.setChildren(secondNode.left, secondNode.right);
+
+        log('Swapped node', firstNode.id, 'with', secondNode.id);
+    };
+
+    /**
+     * Inserts a node with new symbol by adding children to old 'NYT' node.
+     * Left child becomes the new NYT node, right holds the new symbol.
+     *
+     * @param {string} symbol Single character to be encoded.
+     */
+    addNode = function (symbol) {
+        var newNYTNode,
+            symbolNode;
+
+        if (!isTreeFull && (Object.keys(symbols).length < alphabetSize)) {
+            symbolNode = new Node(NYTNode, symbol, NYTNode.id - 1);
+            newNYTNode = new Node(NYTNode, null, NYTNode.id - 2);
+            NYTNode.setChildren(newNYTNode, symbolNode);
+            NYTNode = newNYTNode;
+
+            nodes.push(newNYTNode);
+            nodes.push(symbolNode);
+
+            symbols[symbol] = symbol.charCodeAt(0).toString(2).length;
+        } else {
+            isTreeFull = true;
+            log('ERROR: No more symbols can be added to the tree.');
+        }
+    };
+
+    /**
+     * Returns node with the highest ID in the block (nodes with the same weight).
+     *
+     * @param {number} weight Weight of a block.
+     * @returns {Node} Node with the highest ID.
+     */
+    getHighestIdInBlock = function (weight) {
+        var i,
+            l = nodes.length,
+            highest = -1,
+            highestNode,
+            tmp;
+
+        for (i = 0; i < l; i++) {
+            tmp = nodes[i];
+            if ((tmp.weight === weight) && tmp.id > highest) {
+                highest = tmp.id;
+                highestNode = tmp;
+            }
+        }
+        return highestNode;
+    };
+
+    /**
+     * Returns Huffman code for a given node based on a current encoding tree.
+     *
+     * @param {Node} node Node for which code should be returned.
+     * @returns {string} Encoded character.
+     */
+    getHuffmanCode = function (node) {
+        var arr = [],
+            prev;
+
+        while (node !== root) {
+            prev = node;
+            node = node.parent;
+            arr.push((node.left === prev) ? 0 : 1);
+        }
+
+        return arr.reverse().join('');
+    };
+
+    return {
+
+        /**
+         * Encodes single character.
+         * @param {string} char Single character to be encoded.
+         * @returns {string} Encoded value.
+         */
+        encode: function (char) {
+            return encodeCharacter(char);
+        },
+
+        /**
+         * Returns the root node of the tree.
+         * @returns {AdaptiveHuffman.Node} Root node.
+         */
+        getRoot: function () {
+            return root;
+        },
+
+        /**
+         * Returns approximate compression ratio.
+         *
+         * @returns {string} Compression ratio percentage.
+         */
+        getCompressionRatio: function () {
+            return (compInput / compOutput).toFixed(2);
+        }
+    };
+};
---
+++ js/huffmanvis.js
@@ -0,0 +1,196 @@
+/**
+ * Initializes visualisation for Huffman encoder.
+ */
+function initVis() {
+    'use strict';
+
+    // unbind previous listener
+    document.removeEventListener('log', updateTree, false);
+
+    // get the root of the encoding tree
+    root = HuffmanInstance.getRoot();
+    root.parent = root;
+    root.px = root.x;
+    root.py = root.y;
+
+    updateTree();
+    document.addEventListener('log', updateTree, false);
+
+}
+
+/**
+ * Updates tree visualisation from the root node.
+ */
+function updateTree() {
+    'use strict';
+
+    update(root);
+}
+
+var m = [20, 120, 20, 120],
+    w = 1280 - m[1] - m[3],
+    h = 800 - m[0] - m[2],
+    i = 0,
+    root,
+    tree = d3.layout.tree()
+        .size([h, w]),
+    diagonal = d3.svg.diagonal()
+        .projection(function (d) {
+            'use strict';
+
+            return [d.y, d.x];
+        }),
+    svg = d3.select('#svg-container').append('svg:svg')
+        .attr('width', w + m[1] + m[3])
+        .attr('height', h + m[0] + m[2])
+        .append('svg:g')
+        .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+function update(source) {
+    'use strict';
+
+    var duration = 800,
+        tmp = parseInt(window.animDurationEl.value, 10);
+
+    if (!isNaN(tmp)) { // defined in huffman.htm
+        duration = tmp;
+    }
+
+    var nodes = tree.nodes(root).reverse();
+
+    var node = svg.selectAll('g.node')
+        .data(nodes, function (d) {
+            return d.id;
+        });
+
+    var nodeEnter = node.enter().append('svg:g')
+        .attr('class', 'node')
+        .attr('transform', function () {
+            return 'translate(' + source.y0 + ',' + source.x0 + ')';
+        });
+
+    nodeEnter.append('svg:circle')
+        .attr('r', 2)
+        .style('fill', function (d) {
+            return (d.symbol) ? 'blue' : '#fff';
+        });
+
+    nodeEnter.append('svg:text')
+        .attr('x', -8)
+        .attr('y', -8)
+        .text(function (d) {
+            return d.weight;
+        })
+        .attr('class', 'weight');
+
+    nodeEnter.append('svg:text')
+        .attr('x', 6)
+        .attr('y', 5)
+        .text(function (d) {
+            return (!d.symbol && !d.left) ? 'NYT' : d.symbol;
+        })
+        .attr('class', 'symbol');
+
+    nodeEnter.append('svg:text')
+        .attr('x', -8)
+        .attr('y', 20)
+        .text(function (d) {
+            return d.id;
+        })
+        .attr('class', 'id');
+
+
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr('transform', function (d) {
+            return 'translate(' + d.y0 + ',' + d.x0 + ')';
+        });
+
+    nodeUpdate.select('circle')
+        .attr('r', 4.5)
+        .style('fill', function (d) {
+            return d.symbol ? '#777' : '#fff';
+        });
+
+    var changedSymbols = node.select('.symbol')
+        .text(function (d) {
+            return (!d.symbol && !d.left) ? 'NYT' : d.symbol;
+        })
+        .filter(function(d) {
+            return d.prevSymbol !== d.symbol;
+        });
+
+    if(changedSymbols[0].length){
+        changedSymbols.transition()
+            .duration(duration * 2)
+            .styleTween("stroke", function() {
+                return d3.interpolate("orange", "black");
+            })
+            .styleTween("stroke-width", function() {
+                return d3.interpolate(7, 1);
+            });
+    }
+
+    node.select('.weight')
+        .filter(function(d) {
+            return d.prevWeight !== d.weight;
+        })
+        .transition()
+        .duration(duration * 2)
+        .styleTween("stroke-width", function() {
+            return d3.interpolate(5, 1);
+        })
+        .styleTween("stroke", function() {
+            return d3.interpolate("green", "blue");
+        })
+        .text(function (d) {
+            return d.weight;
+        });
+
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr('transform', function () {
+            return 'translate(' + source.y + ',' + source.x + ')';
+        })
+        .remove();
+
+    nodeExit.select('circle')
+        .attr('r', 1e-6);
+
+    nodeExit.select('text')
+        .style('fill-opacity', 1e-6);
+
+    var link = svg.selectAll('path.link')
+        .data(tree.links(nodes), function (d) {
+            return d.target.id;
+        });
+
+    link.enter().insert('svg:path', 'g')
+        .attr('class', 'link')
+        .attr('d', function (d) {
+            var o = {x: source.x0, y: source.y0};
+            return diagonal({source: o, target: o});
+        })
+        .transition()
+        .duration(duration)
+        .attr('d', diagonal);
+
+    link.transition()
+        .duration(duration)
+        .attr('d', diagonal);
+
+    link.exit().transition()
+        .duration(duration)
+        .attr('d', function (d) {
+            var o = {x: source.x, y: source.y};
+            return diagonal({source: o, target: o});
+        })
+        .remove();
+
+    nodes.forEach(function (d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+        d.prevWeight = d.weight;
+        d.prevSymbol = d.symbol;
+    });
+}
